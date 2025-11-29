ee2adbce781873f9094a3afeea9db146b5e32226
徐圣灿 <1280939520@qq.com>
2025-11-29 14:13:08 +0800

    JDK使用11

--- mylibrary/src/main/java/com/example/mylibrary/PpyUtil.java
+++ mylibrary/src/main/java/com/example/mylibrary/PpyUtil.java
@@ -1,7 +1,10 @@
 package com.example.mylibrary;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class PpyUtil {
-    public static String getPpyName(){
-        return "ppy名称";
+    public static void toastName(Context context){
+         Toast.makeText(context,"皮皮袁",Toast.LENGTH_SHORT).show();
     }
 }
